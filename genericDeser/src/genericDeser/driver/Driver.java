3f70a761e08713c87986150e97729df9aa410ea5
ShubhamGupta4394 <shubhamgupta4394@gmail.com>
2017-04-21 17:50:46 -0400

    my first commit

---
+++ genericDeser/src/genericDeser/driver/Driver.java
@@ -0,0 +1,16 @@
+
+package genericDeser.driver;
+
+public class Driver{
+
+	public static void main(String args[]) {
+
+		System.out.println("\n The args[0] is: " + args[0]);
+		System.out.println("\n The args[1] is: " + args[1]);
+		System.out.println("\n The args[2] is: " + args[2]);
+
+
+	} // end main(...)
+
+} // end public class Driver
+
