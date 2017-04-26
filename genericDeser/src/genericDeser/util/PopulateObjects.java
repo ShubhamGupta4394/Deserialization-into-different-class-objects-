782385a2c49dffc90d44de7b0fbd2da6d6de9383
ShubhamGupta4394 <shubhamgupta4394@gmail.com>
2017-04-26 00:26:40 -0400

    Fifth commit

--- genericDeser/src/genericDeser/util/PopulateObjects.java
+++ genericDeser/src/genericDeser/util/PopulateObjects.java
@@ -28,6 +28,53 @@ public class PopulateObjects {
 	}
     
 	public void deserObjects(){
-	
+		String str,rslt;
+		String classname = null;
+		String type = null;
+		String value = null;
+		String methodName = null;
+		Class cls = null;
+		Object obj = null;
+		
+		while((str=fp.readLineFromFile())!=null)
+		{
+			rslt = str.replaceAll("[<>]", "");
+		try{
+			if(rslt.contains("fqn")){
+				if(!(classname==null)){
+					if(classname.equalsIgnoreCase("genericDeser.util.First")){
+						if(map1.containsKey(obj)){
+							int count = map1.get(obj);
+							count++;
+							map1.put((First) obj, count);
+							
+						}
+						else{
+							map1.put((First) obj, 1);
+						}
+					}
+					else if(classname.equalsIgnoreCase("genericDeser.util.Second"))
+					{
+						if(map2.containsKey(obj)){
+							int count = map2.get(obj);
+							count++;
+							map2.put((Second) obj, count);
+						}
+						else {
+							map2.put((Second) obj, 1);
+						}
+					}
+				}
+				String arr[] = rslt.split(":");
+				classname = arr[1];
+				cls = Class.forName(classname);
+				obj = cls.newInstance();
+			}
+		
+		}
+		catch(Exception e){
+			System.out.println(e);
+		}
+		}
 	}
 }
